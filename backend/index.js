c3e8b8f19d9586e5c6c1ece1220cdbdcf748bda3
DiegoBergara <diegobergaraj@gmail.com>
2022-07-18 14:05:58 -0300

    use config from env file and eslint installed

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const app = express()
+
+dotenv.config()
+
+const db = require('./src/models')
+db.sequelize.sync()
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and re-sync db.')
+})
 
 const corsOptions = {
-  origin: "http://localhost:8000"
-};
+  origin: 'http://localhost:8000'
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello Babe!" });
-});
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello Babe!' })
+})
 
-const PORT = 8080;
+const PORT = process.env.SERVER_PORT
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
+  console.log(`Server is running on port ${PORT}.`)
+})
