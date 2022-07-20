0bab48c05b1b69bde1275390b3297809edbc1f97
DiegoBergara <diegobergaraj@gmail.com>
2022-07-20 13:30:20 -0300

    tutorials details implemented similar as create view

--- frontend/src/components/TutorialDetails.vue
+++ frontend/src/components/TutorialDetails.vue
@@ -1,55 +1,72 @@
 <template>
-  <div v-if="currentTutorial" class="edit-form">
-    <h4>Tutorial</h4>
-    <form>
-      <div class="form-group">
-        <label for="title">Title</label>
-        <input
-          type="text"
-          class="form-control"
-          id="title"
-          v-model="currentTutorial.title"
-        />
+  <div class="edit-form shadow p-3 bg-light rounded mt-5">
+    <div v-if="currentTutorial">
+      <div class="d-flex border-bottom mb-3">
+        <h4 class="p-2 flex-grow-1">Tutorial Details</h4>
+        <button class="p-2 btn btn-danger mb-2" @click="deleteTutorial">
+          Delete
+        </button>
       </div>
-      <div class="form-group">
-        <label for="description">Description</label>
-        <input
-          type="text"
-          class="form-control"
-          id="description"
-          v-model="currentTutorial.description"
-        />
-      </div>
-      <div class="form-group">
-        <label><strong>Status:</strong></label>
-        {{ currentTutorial.published ? "Published" : "Pending" }}
-      </div>
-    </form>
-    <button
-      class="badge badge-primary mr-2"
-      v-if="currentTutorial.published"
-      @click="updatePublished(false)"
-    >
-      UnPublish
-    </button>
-    <button
-      v-else
-      class="badge badge-primary mr-2"
-      @click="updatePublished(true)"
-    >
-      Publish
-    </button>
-    <button class="badge badge-danger mr-2" @click="deleteTutorial">
-      Delete
-    </button>
-    <button type="submit" class="badge badge-success" @click="updateTutorial">
-      Update
-    </button>
-    <p>{{ message }}</p>
-  </div>
-  <div v-else>
-    <br />
-    <p>Please click on a Tutorial...</p>
+      <form>
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            v-model="currentTutorial.title"
+          />
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <input
+            type="text"
+            class="form-control"
+            id="description"
+            v-model="currentTutorial.description"
+          />
+        </div>
+        <div class="form-group">
+          <label>Video</label>
+          <input
+            class="form-control"
+            id="description"
+            v-model="currentTutorial.videoUrl"
+            name="videoUrl"
+          />
+        </div>
+        <label>I want to public this tutorial as:</label>
+        <div class="form-group">
+          <input
+            type="radio"
+            id="public"
+            :value="false"
+            v-model="currentTutorial.isPrivate"
+          />
+          <label for="public" class="ml-1">Public</label>
+
+          <input
+            type="radio"
+            id="private"
+            class="ml-3"
+            :value="true"
+            v-model="currentTutorial.isPrivate"
+          />
+          <label for="private" class="ml-1">Private</label>
+        </div>
+      </form>
+
+      <button type="submit" class="btn btn-primary" @click="updateTutorial">
+        Update
+      </button>
+      <p>{{ message }}</p>
+    </div>
+    <div v-else>
+      <h4 class="text-center">Tutorial not found</h4>
+      <router-link to="/" class="btn btn-danger text-center w-100 mt-3"
+        >Back to home</router-link
+      >
+    </div>
   </div>
 </template>
 <script>
@@ -64,27 +81,10 @@ export default {
   },
   methods: {
     getTutorial(id) {
-      TutorialsRequests.get(id)
+      TutorialsRequests.getById(id)
         .then((response) => {
           this.currentTutorial = response.data;
           console.log(response.data);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    updatePublished(status) {
-      var data = {
-        id: this.currentTutorial.id,
-        title: this.currentTutorial.title,
-        description: this.currentTutorial.description,
-        published: status,
-      };
-      TutorialsRequests.update(this.currentTutorial.id, data)
-        .then((response) => {
-          console.log(response.data);
-          this.currentTutorial.published = status;
-          this.message = "The status was updated successfully!";
         })
         .catch((e) => {
           console.log(e);
@@ -117,9 +117,3 @@ export default {
   },
 };
 </script>
-<style>
-.edit-form {
-  max-width: 300px;
-  margin: auto;
-}
-</style>
