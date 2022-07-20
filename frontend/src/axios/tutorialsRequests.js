551fd25c19c681a43a2364adb298672a512bf264
DiegoBergara <diegobergaraj@gmail.com>
2022-07-20 01:55:01 -0300

    added axios and created api requests

---
+++ frontend/src/axios/tutorialsRequests.js
@@ -0,0 +1,32 @@
+import Api from "./api";
+
+class TutorialsRequests {
+  createTutorial(data) {
+    return Api.post("/tutorials", data);
+  }
+
+  getAll() {
+    return Api.get(`/tutorials`);
+  }
+
+  getByQuery(title, status) {
+    return Api.get(`/tutorials?title=${title}&status=${status}`);
+  }
+
+  getById(id) {
+    return Api.get(`/tutorials/${id}`);
+  }
+
+  delete(id) {
+    return Api.delete(`/tutorials/${id}`);
+  }
+
+  update(id, data) {
+    return Api.put(`/tutorials/${id}`, data);
+  }
+
+  deleteAll() {
+    return Api.delete(`/tutorials`);
+  }
+}
+export default new TutorialsRequests();
